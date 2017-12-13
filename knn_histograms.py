e2bdb30d10cd68dec90dfcea0c7194b3f4a13c74
Giulio Zhou <giuliozhou8@gmail.com>
2017-12-13 05:04:11 +0000

    Pairwise distance histograms

--- knn_histograms.py
+++ knn_histograms.py
@@ -2,28 +2,48 @@ import numpy as np
 import tensorflow as tf
 import sys
 
-def compute_knn_histograms(data_path, k):
+# Bins is an array of values that denote the bucket boundaries.
+def compute_knn_histograms(data_path, k, bins):
     sess = tf.Session()
     training_set_tensor = tf.placeholder(tf.float32, shape=[None, None])
     test_tensor = tf.placeholder(tf.float32, shape=[None])
     norm_tensor = tf.norm(training_set_tensor - test_tensor, axis=1)
-    top_k_vals_tensor, top_k_idx_tensor = tf.nn.top_k(-norm_tensor, k)
-    top_k_vals_tensor = -top_k_vals_tensor
 
     feature_vectors = np.load(data_path)
-    knn_dists = []
+    hist_counts = np.zeros(len(bins) - 1)
     for i, elem in enumerate(feature_vectors):
         if i % 100 == 0:
             print i
-        top_k_vals = sess.run(top_k_vals_tensor,
-                              feed_dict={training_set_tensor: feature_vectors,
-                                         test_tensor: elem})
-        knn_dists.append(top_k_vals)
-    knn_dists = np.array(knn_dists)
-    hist = np.histogram(knn_dists)
-    return hist
+        norms = sess.run(norm_tensor,
+                         feed_dict={training_set_tensor: feature_vectors,
+                                    test_tensor: elem})
+        hist, bin_edges = np.histogram(norms)
+        hist_counts += hist
+    print(hist_counts)
+    return hist_counts, bins
+
+def compute_all_pairs_histograms(data_path, bins):
+    sess = tf.Session()
+    training_set_tensor = tf.placeholder(tf.float32, shape=[None, None])
+    test_tensor = tf.placeholder(tf.float32, shape=[None])
+    norm_tensor = tf.norm(training_set_tensor - test_tensor, axis=1)
+
+    feature_vectors = np.load(data_path)
+    hist_counts = np.zeros(len(bins) - 1)
+    for i, elem in enumerate(feature_vectors):
+        if i % 100 == 0:
+            print i
+        norms = sess.run(norm_tensor,
+                         feed_dict={training_set_tensor: feature_vectors,
+                                    test_tensor: elem})
+        hist, bin_edges = np.histogram(norms)
+        hist_counts += hist
+    print(hist_counts)
+    return hist_counts, bins
 
 if __name__ == '__main__':
     data_path = sys.argv[1]
     k = int(sys.argv[2])
-    compute_knn_histograms(data_path, k)
+    bins = [0.5 * i for i in range(100)]
+    # compute_knn_histograms(data_path, k, bins)
+    compute_all_pairs_histograms(data_path, k, bins)
